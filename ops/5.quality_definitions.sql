92a45805d5df45e2d1a1f011466d207610c7ab1b
tws101 <tws101@users.noreply.github.com>
2026-08-18 17:28:33 -0500

    Update 5.quality_definitions.sql

--- ops/5.quality_definitions.sql
+++ ops/5.quality_definitions.sql
@@ -144,6 +144,26 @@ update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 15, "prefer
 update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 8, "preferred_size" = 3 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'WORKPRINT' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
 -- --- END op 265
 
+-- --- BEGIN op 266 ( update radarr_quality_definitions "Radarr Quality Definitions" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 180, "preferred_size" = 80 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'Bluray-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 266
+
+-- --- BEGIN op 267 ( update radarr_quality_definitions "Radarr Quality Definitions" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 180, "preferred_size" = 80 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'HDTV-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 267
+
+-- --- BEGIN op 268 ( update radarr_quality_definitions "Radarr Quality Definitions" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 280, "preferred_size" = 130 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'Remux-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 268
+
+-- --- BEGIN op 269 ( update radarr_quality_definitions "Radarr Quality Definitions" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 120, "preferred_size" = 55 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'WEBDL-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 269
+
+-- --- BEGIN op 270 ( update radarr_quality_definitions "Radarr Quality Definitions" )
+update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 120, "preferred_size" = 55 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'WEBRip-2160p' and "min_size" = 0 and "max_size" = 0 and "preferred_size" = 0;
+-- --- END op 270
+
 -- --- BEGIN op 271 ( update radarr_quality_definitions "Radarr Quality Definitions" )
 update "radarr_quality_definitions" set "min_size" = 0, "max_size" = 350, "preferred_size" = 120 where "name" = 'Radarr Quality Definitions' and "quality_name" = 'Bluray-2160p' and "min_size" = 0 and "max_size" = 180 and "preferred_size" = 80;
 -- --- END op 271
